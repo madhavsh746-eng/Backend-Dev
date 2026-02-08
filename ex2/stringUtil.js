de8b194d72399e08ee2e56838fcba7c6760f5417
madhavsh746-eng <madhavsh746@gmail.com>
2026-02-09 00:38:37 +0530

    [madhav].add add the ex2 file

---
+++ ex2/stringUtil.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+function upperCase(str) {
+    return str.toUpperCase();         // example function to capitalize the string
+}
+function reverseStr(str) {
+    return str.split("").reverse().join("");         // example function to reverse the string
+}
+function vowelsCount(str){
+    let vowels=str.match(/[aeiouAEIOU]/g);    //example function to count the no. of vowels in the string
+    return vowels.length;    //returning the count of vowels.
+
+}
+export {upperCase, reverseStr, vowelsCount};
